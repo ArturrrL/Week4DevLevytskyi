e3dd005f2dbbdaf6be09b73771f5a8a01f9655e2
ArturrrL <artik.levitskyi18@gmail.com>
2026-07-13 01:19:23 +0300

    added tests an coverage to wkflw

---
+++ myapp/__init__.py
@@ -0,0 +1,4 @@
+def add(a,b):
+    return a + b
+def sustract(a,b):
+    return a - b
---
+++ tests/test_app.py
@@ -0,0 +1,6 @@
+from myapp.app import add,substract
+def test_add():
+    assert add(2,3) == 5
+
+    def sustract():
+        assert substract(5,3) == 2
