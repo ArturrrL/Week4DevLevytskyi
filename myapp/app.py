0e18f91cea728d1fa793ae68df682d7283f03e16
ArturrrL <artik.levitskyi18@gmail.com>
2026-07-13 01:53:47 +0300

    fix typos

--- myapp/app.py
+++ myapp/app.py
@@ -1,4 +1,4 @@
 def add(a,b):
     return a + b
-def sustract(a,b):
+def subtract(a,b):
     return a - b
--- tests/test_app.py
+++ tests/test_app.py
@@ -1,6 +1,6 @@
-from app.app import add,substract
+from myapp.app import add,subtract
 def test_add():
     assert add(2,3) == 5
 
-    def sustract():
-        assert substract(5,3) == 2
+    def subtract():
+        assert subtract(5,3) == 2
